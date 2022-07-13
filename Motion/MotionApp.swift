28315360e3fa241bf27e9e9acde023e2d81bca52
IgorDenisiuk <73896271+IgorDenisiuk@users.noreply.github.com>
2022-07-13 10:16:58 +0300

    Initial Commit

---
+++ Motion/MotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotionApp.swift
+//  Motion
+//
+//  Created by Igor Denisiuk on 13.07.22.
+//
+
+import SwiftUI
+
+@main
+struct MotionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
